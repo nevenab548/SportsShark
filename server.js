aa2c1f5b4310e9efdd011f389d0ede78410e5fbb
AnitaB24 <abrandic1@gmail.com>
2022-02-06 22:34:25 +0100

    obrisano

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
 //register view engine
 app.set('view engine','ejs');
 
-app.get('/',(req,res)=>
+/*app.get('/',(req,res)=>
 {
     res.render('index');
-});
+});*/
 app.get('/about',(req,res)=>
 {
      res.render('about');
